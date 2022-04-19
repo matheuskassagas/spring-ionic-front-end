32f5229954355fa809d2457a5904fd063ef0e0ae
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-04-19 19:30:33 -0300

    mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,6 @@
 //definição de configurações da API
 
 export const API_CONFIG = {
-  baseUrl: "http://localhost:8080" //url base do springboot
+  baseUrl: "http://localhost:8080", //url base do springboot
+  bucketBaseUrl: "https:/s3-sa-east-1.amazonaws.com//curso-spring-ionic-nelio-2022"
 }
--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { CategoriaDTO } from '../../models/categoria.dto';
 import { CategoriaService } from '../../services/domain/categoria.service';
 
 
@@ -10,6 +12,10 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategoriaDTO[];
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
@@ -19,7 +25,7 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll() //chamada assincrona
     .subscribe(response => { //inscrever para quando a resposta chegar eu excultar
-      console.log(response)
+      this.items = response;
     },
     error => {
       console.log(error);
